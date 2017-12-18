b289bf49248b005adea4833d3d7b043c70cae05e
Sun Qingyao <sunqingyao19970825@icloud.com>
2017-12-18 22:40:27 +0800

    survey/sampling.py: assumed all buildings have 31 dorms per floor & improved building number mapping

--- survey/sampling.py
+++ survey/sampling.py
@@ -2,26 +2,19 @@
 from random import seed, sample
 
 
-# Number of dormitories each floor in all dormitory buildings
-# TODO: Figure this out
-dorms_per_floor_list = [20, 20, 20, 20, 30, 30, 30, 25, 25, 27, 34, 19, 25, 21]
-floors_per_building = 6
-dorms_sum = sum(dorms_per_floor_list) * floors_per_building
+# Configuration Variables
+BUILDING_COUNT = 20
+FLOORS_PER_BUILDING = 6
+DORMS_PER_FLOOR = 31
 
 
 def main(size):
     """Construct a SRSWOR sample of dormitories, and write it to a file named "sample.txt"
 
-    Arguments:
-    size -- the size of the SRS sample
-
-    Assumptions:
-    + Floors in one dormitory building are laid out identically.
-    + Each floor has less than one hundred dormitories.
-    + Each dormitory holds exactly four students (no empty dorms).
-    + Every student lives in a dormitory.
+    :param size: the size of the SRS sample
     """
-    sample_ = sample(range(dorms_sum), size)
+    dorms_count = DORMS_PER_FLOOR * FLOORS_PER_BUILDING * BUILDING_COUNT
+    sample_ = sample(range(dorms_count), size)
 
     with open('sample.txt', 'w') as f:
         f.write('\n  === DRAFT ===  \n\n')
@@ -30,27 +23,38 @@ def main(size):
         sample_ = humanize(sample_)
         output = '\n'.join('{:4d} | {}'.format(*k) for k in enumerate(sample_))
         f.write(output)
-        # Extra newline for Unix utilities
+        # UNIX/Linux terminal compatibility.
         f.write('\n')
 
 
 def humanize(sample_):
-    """
-    Make indexes of a sample human-readable (e.g. 1753 -> 12-411)
+    """Make indexes of a sample human-readable, and sort them in ascending order
+
+    :param sample_: a list of indexes forming an sample
+    :return: humanized and sorted indexes
     """
     humanized = []
+    # e.g. [1753, 1754] -> [12-411, 12-412]
     for idx in sample_:
-        for building, dorms_per_floor in enumerate(dorms_per_floor_list):
-            dorms_per_building = dorms_per_floor * floors_per_building
-            if idx < dorms_per_building:
-                break
-            idx -= dorms_per_building
-        floor, dorm = divmod(idx, dorms_per_floor)
-        humanized.append('{:2d}-{}{:02d}'.format(building + 1, floor + 1, dorm + 1))
+        dorms_per_building = DORMS_PER_FLOOR * FLOORS_PER_BUILDING
+        building, idx_in_building = divmod(idx, dorms_per_building)
+        floor, dorm = divmod(idx_in_building, DORMS_PER_FLOOR)
+        humanized.append('{:2d}-{}{:02d}'.format(map_bld(building), floor + 1, dorm + 1))
     humanized.sort()
     return humanized
 
 
+def map_bld(building):
+    """Maps the index of a dorm building to its number
+
+    :param building: the index of a dorm building
+    :return: the number of a dorm building
+    """
+    # Strangely, there is no dorm building No. 17 & 18
+    # So 15 -> 16, 16 -> 19, 17 -> 20, ...
+    return building + 1 if building < 16 else building + 3
+
+
 if __name__ == '__main__':
     seed()
-    main(1000)
+    main(200)
